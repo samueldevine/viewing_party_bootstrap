543fa059ecf73858fe240a6bc95be964b204cd13
Sam Devine <samuel.will.devine@gmail.com>
2021-12-01 10:55:56 -0700

    refactoring routes file

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,9 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :show, :create]
 
   get '/register', to: 'users#new'
-  post '/users', to: 'users#create'
+  # post '/users', to: 'users#create'
   get '/users/:user_id', to: 'users#show'
   get '/users/:id/discover', to: 'users#discover'
 
-  get '/users/:id/movies', to: 'movies#top_rated'
-  post '/users/:id/movies', to: 'movies#search'
+  get '/users/:id/movies', to: 'movies#search'
 end
