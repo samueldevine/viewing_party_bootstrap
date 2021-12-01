177eb8425181deae01c431a9677a3656c4f0311e
Sam Devine <samuel.will.devine@gmail.com>
2021-11-30 19:21:44 -0700

    fix: blank search re-renders discover page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,14 +1,19 @@
 class MoviesController < ApplicationController
   def search
-    @query = params[:search]
+    if params[:search] != ''
+      @query = params[:search]
 
-    conn = Faraday.new(url: "https://api.themoviedb.org/3")
-    response = conn.get("search/movie", { query: params[:search], api_key: ENV['movie_api_key'] })
+      conn = Faraday.new(url: "https://api.themoviedb.org/3")
+      response = conn.get("search/movie", { query: params[:search], api_key: ENV['movie_api_key'] })
 
-    data = JSON.parse(response.body, symbolize_names: true)
-    @movies = data[:results][0..19]
+      data = JSON.parse(response.body, symbolize_names: true)
+      @movies = data[:results][0..19]
 
-    render :movies
+      render :movies
+    else
+      @user = User.find(params[:id])
+      render 'users/discover'
+    end
   end
 
   def top_rated
--- spec/features/movies/search_movies_spec.rb
+++ spec/features/movies/search_movies_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe 'Movie Search' do
-  describe 'happy path' do
-    let(:bob) { User.create(name: 'Bob Belcher', email: 'bburger@yahoo.com') }
+  let(:bob) { User.create(name: 'Bob Belcher', email: 'bburger@yahoo.com') }
 
+  describe 'happy path' do
     it 'allows users to search for a movie by title' do
       visit "/users/#{bob.id}/discover"
 
@@ -24,6 +24,17 @@ RSpec.describe 'Movie Search' do
       expect(page.status_code).to eq 200
       expect(page).to have_content('Fight Club')
       expect(page).to have_content('Vote Average:', count: 20)
+    end
+  end
+
+  describe 'sad path' do
+    it 'does nothing if no query is entered' do
+      visit "/users/#{bob.id}/discover"
+      click_button 'Find Movies'
+
+      expect(page.status_code).to eq 200
+      expect(current_path).to eq "/users/#{bob.id}/movies"
+      expect(page).to have_button 'Find Movies'
     end
   end
 end
