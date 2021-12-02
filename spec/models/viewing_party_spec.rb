731e4bd0ba6580c836d19d960cfefa9d39663d87
Sam Devine <samuel.will.devine@gmail.com>
2021-12-02 15:40:42 -0700

    test: viewing party validations, no duration yet
    Co-authored-by: cvalesares <84933281+cvalesares@gmail.com>

--- spec/models/viewing_party_spec.rb
+++ spec/models/viewing_party_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe ViewingParty, type: :model do
     it { should have_many :user_parties }
     it { should have_many :users}
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:movie_id) }
+    it { should validate_presence_of(:host_id) }
+    it { should validate_presence_of(:date) }
+    it { should validate_presence_of(:start_time) }
+  end
 end
