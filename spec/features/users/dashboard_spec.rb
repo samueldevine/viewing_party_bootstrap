79a9ef5fb50e1800a20a63d041291d5033072cc7
Sam Devine <samuel.will.devine@gmail.com>
2021-12-02 16:35:00 -0700

    rename users#show to users#dashboard for consistency

--- spec/features/users/dashboard_spec.rb
+++ spec/features/users/dashboard_spec.rb
@@ -1,26 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe "A User's Dashboard" do
-  let(:sam) { User.create(name: 'Sam', email: 'sam@sam.com') }
+  let(:bob) { User.create(name: 'Bob Belcher', email: 'bburger@yahoo.com') }
 
   it 'displays the name of the user' do
-    visit user_path(sam)
+    visit "/users/#{bob.id}"
 
-    expect(page).to have_content "Sam's Dashboard"
+    expect(page).to have_content "Bob Belcher's Dashboard"
   end
 
   it 'has a button to discover movies' do
-    visit user_path(sam)
+    visit "/users/#{bob.id}"
 
     expect(page).to have_button 'Discover Movies'
 
     click_on 'Discover Movies'
 
-    expect(current_path).to eq "/users/#{sam.id}/discover"
+    expect(current_path).to eq "/users/#{bob.id}/discover"
   end
 
   it 'has a section that lists viewing parties' do
-    visit user_path(sam)
+    visit "/users/#{bob.id}"
 
     within '#viewing-parties' do
       expect(page).to have_content 'Viewing Parties'
