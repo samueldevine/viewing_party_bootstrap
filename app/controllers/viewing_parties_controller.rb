c516cd42b017a7edb859085a9924e81b48eb842e
Sam Devine <samuel.will.devine@gmail.com>
2021-12-03 13:20:03 -0700

    minimum viewing party duration

--- app/controllers/viewing_parties_controller.rb
+++ app/controllers/viewing_parties_controller.rb
@@ -6,20 +6,28 @@ class ViewingPartiesController < ApplicationController
   end
 
   def create
-    viewing_party = ViewingParty.create!(
-      movie_id: params[:movie_id],
-      host_id: params[:id],
-      date: params[:date],
-      start_time: params[:start_time],
-      duration: params[:duration]
-      )
+    if params[:duration] < params[:runtime]
+      flash[:notice] = "Please choose a duration longer than the movie's runtime."
+      @user = User.find(params[:id])
+      @users = User.all
+      @movie = MovieFacade.movie_details(params[:movie_id])
+      render :new
+    else
+      viewing_party = ViewingParty.create!(
+        movie_id: params[:movie_id],
+        host_id: params[:id],
+        date: params[:date],
+        start_time: params[:start_time],
+        duration: params[:duration]
+        )
 
-    params[:invitations].each do |inv|
-      if inv[1] == "1"
-        viewing_party.users << User.find(inv[0])
+      params[:invitations].each do |inv|
+        if inv[1] == "1"
+          viewing_party.users << User.find(inv[0])
+        end
       end
-    end
 
-    redirect_to "/users/#{params[:id]}"
+      redirect_to "/users/#{params[:id]}"
+    end
   end
 end
--- spec/features/viewing_parties/new_spec.rb
+++ spec/features/viewing_parties/new_spec.rb
@@ -46,4 +46,21 @@ RSpec.describe 'New Viewing Party' do
       expect(page).to_not have_content(movie.title)
     end
   end
+
+  describe 'sad path' do
+    it 'prevents a party from being created with a shorter duration than the movie', :vcr do
+      movie = MovieFacade.movie_details(550)
+      visit "/users/#{@linda.id}/movies/#{movie.id}/viewing-party/new"
+
+      fill_in "duration", with: (movie.runtime - 5)
+      fill_in "date", with: "06/12/2022"
+      fill_in "start_time", with: "12:00"
+      check "[invitations][#{@linda.id}]"
+      check "[invitations][#{@bob.id}]"
+      click_button "Create Party"
+
+      expect(current_path).to eq "/users/#{@linda.id}/movies/#{movie.id}/viewing-party"
+      expect(page).to have_content "Please choose a duration longer than the movie's runtime."
+    end
+  end
 end
