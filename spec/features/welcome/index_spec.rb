463233c6ec41b1aeabeab41bbc643f05792c09c3
Sam Devine <samuel.will.devine@gmail.com>
2021-12-15 17:36:27 -0500

    remove extra comments

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -19,23 +19,4 @@ RSpec.describe 'The Landing Page' do
 
     expect(current_path).to eq '/login'
   end
-  # it 'lists all current users' do
-  #   bob = User.create(name: 'Bob Belcher', email: 'bburger@yahoo.com', password: 'burger', password_confirmation: 'burger')
-  #   visit root_path
-
-  #   within '#existing-users' do
-  #     expect(page).to have_content(bob.email)
-  #   end
-  # end
-
-  # it 'links to each users dashboard' do
-  #   bob = User.create(name: 'Bob Belcher', email: 'bburger@yahoo.com', password: 'burger', password_confirmation: 'burger')
-  #   visit root_path
-
-  #   within '#existing-users' do
-  #     click_on 'bburger@yahoo.com'
-  #   end
-
-  #   expect(current_path).to eq "/users/#{bob.id}"
-  # end
 end
